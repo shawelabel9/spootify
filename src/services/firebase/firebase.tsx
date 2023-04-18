612803a350f257679044e7a71cac571e23ea32c4
Abel Shawel <shawelabel9@gmail.com>
2023-04-18 19:27:19 +0300

    added tailwindcss to the project

---
+++ src/services/firebase/firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeRb0YgkdKYDQ_7TYaYrJcI50NfT2Kw5w",
+  authDomain: "music-library-a9d7d.firebaseapp.com",
+  projectId: "music-library-a9d7d",
+  storageBucket: "music-library-a9d7d.appspot.com",
+  messagingSenderId: "171830499625",
+  appId: "1:171830499625:web:b9b55d9cc2a6d240397f38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
